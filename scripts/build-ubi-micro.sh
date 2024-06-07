378c128135377861120521d7db4f36ace9e5f77c
Rodrigo Bassil <rodrigo.bassil@dell.com>
2024-06-07 17:23:49 +0000

    Updating dependencies to UBI micro image.

--- scripts/build-ubi-micro.sh
+++ scripts/build-ubi-micro.sh
@@ -4,4 +4,4 @@ micromount=$(buildah mount $microcontainer)
 dnf install --installroot $micromount --releasever=8 --nodocs --setopt install_weak_deps=false --setopt=reposdir=/etc/yum.repos.d/ rpm -y
 dnf clean all --installroot $micromount
 buildah umount $microcontainer
-buildah commit $microcontainer ${2}
+buildah commit $microcontainer rep-ubimicro
