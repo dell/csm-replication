67ed78e65a18583569f3a2e173da2ef243079342
Lau, Luke <luke.lau1@dell.com>
2024-11-14 14:33:18 -0500

    fmt mocks

--- repctl/mocks/ClientInterface.go
+++ repctl/mocks/ClientInterface.go
@@ -235,43 +235,42 @@ func (_m *ClientInterface) Update(ctx context.Context, obj client.Object, opts .
 
 // GroupVersionKindFor provides a mock function with given fields: obj
 func (_m *ClientInterface) GroupVersionKindFor(obj runtime.Object) (schema.GroupVersionKind, error) {
-    ret := _m.Called(obj)
+	ret := _m.Called(obj)
 
-    var r0 schema.GroupVersionKind
-    if rf, ok := ret.Get(0).(func(runtime.Object) schema.GroupVersionKind); ok {
-        r0 = rf(obj)
-    } else {
-        r0 = ret.Get(0).(schema.GroupVersionKind)
-    }
+	var r0 schema.GroupVersionKind
+	if rf, ok := ret.Get(0).(func(runtime.Object) schema.GroupVersionKind); ok {
+		r0 = rf(obj)
+	} else {
+		r0 = ret.Get(0).(schema.GroupVersionKind)
+	}
 
-    var r1 error
-    if rf, ok := ret.Get(1).(func(runtime.Object) error); ok {
-        r1 = rf(obj)
-    } else {
-        r1 = ret.Error(1)
-    }
+	var r1 error
+	if rf, ok := ret.Get(1).(func(runtime.Object) error); ok {
+		r1 = rf(obj)
+	} else {
+		r1 = ret.Error(1)
+	}
 
-    return r0, r1
+	return r0, r1
 }
 
 // IsObjectNamespaced provides a mock function with given fields: obj
 func (_m *ClientInterface) IsObjectNamespaced(obj runtime.Object) (bool, error) {
-    ret := _m.Called(obj)
+	ret := _m.Called(obj)
 
-    var r0 bool
-    if rf, ok := ret.Get(0).(func(runtime.Object) bool); ok {
-        r0 = rf(obj)
-    } else {
-        r0 = ret.Get(0).(bool)
-    }
+	var r0 bool
+	if rf, ok := ret.Get(0).(func(runtime.Object) bool); ok {
+		r0 = rf(obj)
+	} else {
+		r0 = ret.Get(0).(bool)
+	}
 
-    var r1 error
-    if rf, ok := ret.Get(1).(func(runtime.Object) error); ok {
-        r1 = rf(obj)
-    } else {
-        r1 = ret.Error(1)
-    }
+	var r1 error
+	if rf, ok := ret.Get(1).(func(runtime.Object) error); ok {
+		r1 = rf(obj)
+	} else {
+		r1 = ret.Error(1)
+	}
 
-    return r0, r1
+	return r0, r1
 }
-
