cfc1b3de7058d4a64dc2b1f7e73965cebb05e728
Samihan Deshmukh <samihan.deshmukh@dell.com>
2025-03-19 17:17:50 +0530

    fix: fiixing linting issues in yaml

--- cmd/csi-replicator/main_test.go
+++ cmd/csi-replicator/main_test.go
@@ -378,7 +378,7 @@ func TestSetupConfigMapWatcher(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(tt.name, func(_ *testing.T) {
 			mgr := &ReplicatorManager{}
 			mgr.setupConfigMapWatcher(tt.loggerConfig)
 		})
