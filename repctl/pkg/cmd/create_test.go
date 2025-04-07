4d5a474118f1548523b0d24b81421a5a52acf305
Trevor Dawe <trevor.dawe@dell.com>
2025-04-07 19:27:34 +0000

    Add support for csi-nfs parameter in StorageClass for PowerStore

--- repctl/pkg/cmd/create_test.go
+++ repctl/pkg/cmd/create_test.go
@@ -123,8 +123,9 @@ func (suite *CreateTestSuite) TestCreateSCs() {
 				Source: "WX-0001",
 				Target: "WX-0002",
 			},
-			Rpo:  "Five_Minutes",
-			Mode: "ASYNC",
+			Rpo:    "Five_Minutes",
+			Mode:   "ASYNC",
+			CsiNfs: "RWX",
 		},
 	}
 
