6fb229c7bb9c5bd42773e7b3fc3bf8cb62058212
Lau, Luke <luke.lau1@dell.com>
2025-09-04 12:42:04 -0400

    adding tests for repctl storageclass generation

---
+++ repctl/test/storageclass/generate_test.go
@@ -0,0 +1,176 @@
+package test
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	// The name of the repctl binary.
+	repctlCLI = "repctl"
+	// The repctl "create" command used to create k8s resources.
+	cmdCreate = "create"
+	// Name of the resource provided to the repctl "create" command for creating
+	// a storage class.
+	resourceStorageClass = "sc"
+	// Option used when creating StorageClasses with repctl that allows
+	// user to supply a custom repctl config file for generating replication-enabled
+	// storage classes.
+	optFromConfig = "--from-config"
+	// Option provided to repctl to generate output but skip applying.
+	optDryRun = "--dry-run"
+
+	// name of the environment variable used to provide a path to repctl executable.
+	envRepctlPath = "REPCTL_PATH"
+)
+
+// relative location of the directory containing the config files fed to `repctl create sc` command.
+var configFileDir = "testdata"
+
+// getRepctlPath searches for a repctl executable and returns a file path to the executable.
+// If the executable cannot be found, an error is returned.
+// Search paths include csm-replication/repctl, the path provided by environment variable REPCTL_PATH,
+// and PATH, in respective order.
+func getRepctlPath() (path string, err error) {
+	fmt.Println("looking for the repctl executable in csm-replication/repctl")
+
+	// Try to locate repctl in the default build output location
+	_, err = os.Stat("../../repctl")
+	if err == nil {
+		return "../../repctl", nil
+	}
+	fmt.Printf("error looking for repctl in csm-replication/repctl: %s\n", err.Error())
+
+	// Try to locate repctl using the path provided via REPCTL_PATH
+	fmt.Println("looking for repctl using the path provided via REPCTL_PATH")
+	path = filepath.Join(os.Getenv(envRepctlPath), repctlCLI)
+	_, err = os.Stat(path)
+	if err == nil {
+		return path, nil
+	}
+	fmt.Printf("error looking for repctl in REPCTL_PATH: %s\n", err.Error())
+
+	// Try to locate repctl as part of one of the paths in PATH
+	fmt.Println("looking for repctl in PATH")
+	path, err = exec.LookPath(repctlCLI)
+	if err == nil {
+		return path, nil
+	}
+	fmt.Printf("error looking for repctl in PATH: %s\n", err.Error())
+
+	return "", fmt.Errorf("unable to locate repctl. All methods were exhausted")
+}
+
+// use repctl to generate power scale storage class and compare
+// the output to the expected output
+func TestGeneratePowerScaleStorageClass(t *testing.T) {
+	repctl, err := getRepctlPath()
+	if err != nil {
+		t.Errorf("encountered error while searching for repctl: %s", err.Error())
+		t.SkipNow()
+	}
+
+	type args struct {
+		templateName string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "generate a csi-powerscale replication storage class with all options populated",
+			args: args{
+				templateName: "powerscale",
+			},
+			want: powerscaleStorageClass,
+		},
+	}
+
+	for _, tt := range tests {
+		configFilePath := filepath.Join(configFileDir, tt.args.templateName+"_tmpl_values.yaml")
+		generated, err := exec.CommandContext(context.Background(), repctl, cmdCreate, resourceStorageClass, optFromConfig, configFilePath, optDryRun).CombinedOutput()
+		if err != nil {
+			t.Errorf("encountered error while generating storage class: %s", err.Error())
+			t.FailNow()
+		}
+
+		// remove the log message printed by repctl at the beginning of the print out
+		newlineIndex := bytes.IndexRune(generated, '\n')
+		generated = generated[newlineIndex:]
+
+		assert.Equal(t, string(tt.want), string(generated))
+	}
+}
+
+// Expected output when running `repctl create sc --from-config ...` using a powerscale config template.
+// Used to validate repctl is generating the expected storage classes.
+// If the config file is updated with more configuration options, the new output should be reviewed and
+// upon approval, be used to replace these values.
+const powerscaleStorageClass = `
+# yamllint disable-file
+# This file is not valid YAML because it is a Helm template
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+  name: isilon-replication
+provisioner: csi-isilon.dellemc.com
+reclaimPolicy: Delete
+volumeBindingMode: Immediate
+parameters:
+  replication.storage.dell.com/isReplicationEnabled: "true"
+  replication.storage.dell.com/remoteStorageClassName: isilon-replication
+  replication.storage.dell.com/remoteClusterID: target
+  replication.storage.dell.com/rpo: Five_Minutes
+  replication.storage.dell.com/ignoreNamespaces: "false"
+  replication.storage.dell.com/volumeGroupPrefix: csi
+  replication.storage.dell.com/remoteSystem: cluster-2
+  replication.storage.dell.com/remoteRGRetentionPolicy: Retain
+  replication.storage.dell.com/remotePVRetentionPolicy: Retain
+  replication.storage.dell.com/remoteAccessZone: System
+  replication.storage.dell.com/remoteAzServiceIP: 192.168.1.2
+  replication.storage.dell.com/remoteAzNetwork: 192.168.3.0/24
+  replication.storage.dell.com/remoteRootClientEnabled: "false"
+  AccessZone: System
+  AzServiceIP: 192.168.1.1
+  AzNetwork: 192.168.2.0/24
+  IsiPath: /ifs/data/csi
+  RootClientEnabled: "false"
+  ClusterName: cluster-1
+# yamllint disable-file
+# This file is not valid YAML because it is a Helm template
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+  name: isilon-replication
+provisioner: csi-isilon.dellemc.com
+reclaimPolicy: Delete
+volumeBindingMode: Immediate
+parameters:
+  replication.storage.dell.com/isReplicationEnabled: "true"
+  replication.storage.dell.com/remoteStorageClassName: isilon-replication
+  replication.storage.dell.com/remoteClusterID: source
+  replication.storage.dell.com/rpo: Five_Minutes
+  replication.storage.dell.com/ignoreNamespaces: "false"
+  replication.storage.dell.com/volumeGroupPrefix: csi
+  replication.storage.dell.com/remoteSystem: cluster-1
+  replication.storage.dell.com/remoteRGRetentionPolicy: Retain
+  replication.storage.dell.com/remotePVRetentionPolicy: Retain
+  replication.storage.dell.com/remoteAccessZone: System
+  replication.storage.dell.com/remoteAzServiceIP: 192.168.1.1
+  replication.storage.dell.com/remoteAzNetwork: 192.168.2.0/24
+  replication.storage.dell.com/remoteRootClientEnabled: "false"
+  AccessZone: System
+  AzServiceIP: 192.168.1.2
+  AzNetwork: 192.168.3.0/24
+  IsiPath: /ifs/data/csi
+  RootClientEnabled: "false"
+  ClusterName: cluster-2
+`
